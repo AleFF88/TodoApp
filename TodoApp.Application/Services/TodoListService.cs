f5f0d51b308d396fe9d76fcf4d4830c9e734b010
AleFF88 <1871847@gmail.com>
2026-03-31 13:37:00 +0300

    Implement CreateListAsync on Application level

---
+++ TodoApp.Application/Services/TodoListService.cs
@@ -0,0 +1,21 @@
+﻿using TodoApp.Domain.Entities;
+using TodoApp.Domain.Repositories;
+
+namespace TodoApp.Application.Services {
+    public class TodoListService {
+        private readonly ITodoListRepository _repository;
+
+        public TodoListService(ITodoListRepository repository) {
+            _repository = repository;
+        }
+
+        // Создание нового списка
+        public async Task<Guid> CreateListAsync(string title, Guid ownerId) {
+            var id = Guid.NewGuid();
+            var todoList = new TodoList(id, title, ownerId);
+
+            await _repository.AddAsync(todoList);
+            return id;
+        }
+    }
+}
