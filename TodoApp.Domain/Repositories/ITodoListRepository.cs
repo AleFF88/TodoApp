8b94097c12ded024bf30b96ce77f43bebccba7fd
AleFF88 <1871847@gmail.com>
2026-03-31 12:58:43 +0300

    Implement Repositories on Domain level

---
+++ TodoApp.Domain/Repositories/ITodoListRepository.cs
@@ -0,0 +1,11 @@
+﻿using TodoApp.Domain.Entities;
+
+namespace TodoApp.Domain.Repositories {
+    public interface ITodoListRepository {
+        Task AddAsync(TodoList todoList);
+        Task UpdateAsync(TodoList todoList);
+        Task DeleteAsync(Guid id);
+        Task<TodoList?> GetByIdAsync(Guid id);
+        Task<IEnumerable<TodoList>> GetPagedAsync(Guid userId, int pageNumber, int pageSize);
+    }
+}
