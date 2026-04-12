45d518c98b173f450af8cce9327551877270ffdb
AleFF88 <1871847@gmail.com>
2026-04-12 19:34:26 +0300

    Implemented repository for TodoList

---
+++ TodoApp.Infrastructure/Repositories/TodoListRepository.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using TodoApp.Domain.Entities;
+using TodoApp.Domain.Repositories;
+using TodoApp.Infrastructure;
+
+public class TodoListRepository : ITodoListRepository {
+    private readonly IMongoCollection<TodoList> _collection;
+
+    public TodoListRepository(MongoDbContext context) {
+        _collection = context.TodoLists;
+    }
+
+    public async Task AddAsync(TodoList todoList) {
+        await _collection.InsertOneAsync(todoList);
+    }
+
+    public async Task UpdateAsync(TodoList todoList) {
+        await _collection.ReplaceOneAsync(l => l.Id == todoList.Id, todoList);
+    }
+
+    public async Task DeleteAsync(Guid id) {
+        await _collection.DeleteOneAsync(l => l.Id == id);
+    }
+
+    public async Task<TodoList?> GetByIdAsync(Guid id) {
+        return await _collection.Find(l => l.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<TodoList>> GetPagedAsync(Guid userId, int pageNumber, int pageSize) {
+        var filter = Builders<TodoList>.Filter.Or(
+            Builders<TodoList>.Filter.Eq(l => l.OwnerId, userId),
+            Builders<TodoList>.Filter.AnyEq(l => l.SharedUserIds, userId)
+        );
+
+        return await _collection.Find(filter)
+            .SortByDescending(l => l.CreatedAt)     
+            .Skip((pageNumber - 1) * pageSize)      
+            .Limit(pageSize)                        
+            .ToListAsync();
+    }
+}
