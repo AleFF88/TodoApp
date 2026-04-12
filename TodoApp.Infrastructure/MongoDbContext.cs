5a525d163cf8d8a2053421fea5cdd15eed391bd1
AleFF88 <1871847@gmail.com>
2026-04-12 18:31:39 +0300

    Implemented MongoDbContext

---
+++ TodoApp.Infrastructure/MongoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Infrastructure {
+    public class MongoDbContext {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(string connectionString, string databaseName) {
+            // Инициализация клиента, стока подключения указана в конфигурации 
+            var client = new MongoClient(connectionString);
+            // Подключение к базе данных, имя базы данных указано в конфигурации
+            _database = client.GetDatabase(databaseName);
+        }
+
+        // Доступ к коллекции документов TodoList
+        public IMongoCollection<TodoList> TodoLists =>
+            _database.GetCollection<TodoList>("TodoLists");
+    }
+}
